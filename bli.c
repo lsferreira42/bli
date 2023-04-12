0fe6c2abbab84e03968d26d59631f55d71474612
Leandro Ferreira <leandrodsferreira@gmail.com>
2023-04-11 21:30:52 -0300

    remove the support for running the code from the command line arguments, now it runs code from stdin and from a file

--- bli.c
+++ bli.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <emscripten.h>
 
-char tape[30000], *ptr = tape;
+char *tape, *ptr;
 
 char brainfuck(char *code, size_t code_len) {
     char output[30000] = {0};
@@ -72,16 +71,93 @@ char brainfuck(char *code, size_t code_len) {
     return 0;
 }
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s \"brainfuck-code\"\n", argv[0]);
+void read_code_from_stdin(char **code, size_t *code_len) {
+    size_t capacity = 4096;
+    *code = malloc(capacity * sizeof(char));
+    if (*code == NULL) {
+        fprintf(stderr, "Memory allocation for code buffer failed\n");
         exit(EXIT_FAILURE);
     }
 
-    char *code = argv[1];
-    size_t code_len = strlen(code);
+    char c;
+    *code_len = 0;
+    while ((c = getchar()) != EOF) {
+        if (*code_len >= capacity) {
+            capacity *= 2;
+            *code = realloc(*code, capacity * sizeof(char));
+            if (*code == NULL) {
+                fprintf(stderr, "Memory reallocation for code buffer failed\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+        (*code)[(*code_len)++] = c;
+    }
+}
+
+void read_code_from_file(const char *file_path, char **code, size_t *code_len) {
+    FILE *file = fopen(file_path, "r");
+    if (file == NULL) {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
+
+    fseek(file, 0, SEEK_END);
+    *code_len = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    *code = malloc(*code_len * sizeof(char));
+    if (*code == NULL) {
+        fprintf(stderr, "Memory allocation for code buffer failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    fread(*code, sizeof(char), *code_len, file);
+    fclose(file);
+}
+
+int is_stdin_empty() {
+    int c = getchar();
+    if (c == EOF) {
+        return 1;
+    }
+    ungetc(c, stdin);
+    return 0;
+}
+
+void show_usage(const char *program_name) {
+    fprintf(stderr, "Usage: %s [-c /path/to/code.bf]\n", program_name);
+    exit(EXIT_FAILURE);
+}
+
+
+
+int main(int argc, char *argv[]) {
+    char *code;
+    size_t code_len;
+
+    if (argc == 1) {
+        if (is_stdin_empty()) {
+            show_usage(argv[0]);
+        }
+        read_code_from_stdin(&code, &code_len);
+    } else if (argc == 3 && strcmp(argv[1], "-c") == 0) {
+        read_code_from_file(argv[2], &code, &code_len);
+    } else {
+        fprintf(stderr, "Usage: %s [-c /path/to/code.bf]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    tape = calloc(30000, sizeof(char));
+    if (tape == NULL) {
+        fprintf(stderr, "Memory allocation for tape failed\n");
+        exit(EXIT_FAILURE);
+    }
+    ptr = tape;
 
     brainfuck(code, code_len);
+
+    free(tape);
+    free(code);
 
     return 0;
 }
